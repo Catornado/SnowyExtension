6630a2b9b11ae06be2ebf576204a58c018cd006f
Catornado <gummy.burgers@gmail.com>
2022-12-06 19:20:57 -0500

    On branch main

--- scripts/background.js
+++ scripts/background.js
@@ -1,27 +1,47 @@
 var snowy = []; 
-var counter = 0;   
-function summonsnowy()
+var counter = 0;  
+//chrome.extension.addListener(tabs)
+function inject(arg)
 {
     let tabbyid;
-    let queryOptions = { active: true, lastFocusedWindow: true };
-    let promise = chrome.tabs.query(queryOptions).then(function(value){
-        chrome.scripting.executeScript({
-            target: {
-                tabId: tabs[0].id,
-            },
-            func: () => {
-                var po = 1;
-            }
-        });
+    //chrome.tabs.getCurrent((tabs) => { tabbyid = tabs.id; console.log(tabs.id) });
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, (tabs) => {
+        tabbyid = tabs[0].id; 
+        console.log(tabbyid);
+        if (arg == "summonsnowy();")
+        {
+            chrome.scripting.executeScript({
+                target: {tabId: tabbyid},
+                func: () => {
+                    if (typeof snowys === "undefined")
+                    {
+                        var snowys = [];
+                        var snowycount = 0;
+                    }
+                    snowys.push("snowy" + snowycount);
+                    snowycount += 1;
+                    console.log("hellooooo");
+                },
+          
+            });
+        }
     });
-
+        
+/*    console.log(tabbyid);
+    chrome.scripting.executeScript({
+        target: {tabId: tabbyid},
+        func: function () {
+            var po = 1;
+        },
+        
+    });
+*/
     console.log("test");
 }
 chrome.runtime.onMessage.addListener((message, callback) => {
     if (message.string === "summonsnowy();")
     {
-        summonsnowy();
-        console.log("test");
+        inject(message.string);
     }
 
 })
