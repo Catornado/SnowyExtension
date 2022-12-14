69c931aaa746fe9e7addf02464c6dd45fe0d9eb4
Catornado <gummy.burgers@gmail.com>
2022-12-13 19:00:51 -0500

    with '#' will be ignored, and an empty message aborts the commit.

--- games/tictactoeai.js
+++ games/tictactoeai.js
@@ -2,7 +2,7 @@ var magicboard = [
     8,1,6,
     3,5,7,
     4,9,2
-];
+];//15
 function emptyslots(loadedboard)
 {
     return loadedboard.filter(function (value) {
@@ -12,7 +12,16 @@ function emptyslots(loadedboard)
 function algorithm(loadedboard, symbol)
 {
     let info = [];
+    
+    let empty = emptyslots(loadedboard);
+    for (let i = 0; i < empty.length; i++)
+    {
+        let currentinfo = {};
+    }
 
+    //subtract result from bad guy from 15
+
+//minimax or magic square
 }
 function playmove(boardstate, alliance)
 {
