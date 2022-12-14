2d4a09d5d46906a9736ca6bcf066120e180af3de
Catornado <gummy.burgers@gmail.com>
2022-12-13 22:18:03 -0500

    On branch main

--- games/tictactoeai.js
+++ games/tictactoeai.js
@@ -9,6 +9,41 @@ function emptyslots(loadedboard)
         return value != "X" && value != "O";
     });
 }
+function win(boardd, symbol)//needs work, it only checks for winners with 0 depth instead of 1
+{
+    for (let i = 0; i < 9; i++)
+    {
+        for (let j = 0; j < 9; j++)
+        {
+            for (let k = 0; k < 9; k++)
+            {
+                if (boardd[i] == "O" && boardd[j] == "O" && board[k] == "O")
+                {
+                    if (magicboard[i] + magicboard[j] + magicboard[k] == 15)
+                    {
+                        if (i != j && i != k && j != k)
+                        {
+                            return 1;
+                        }
+                       
+                    }
+                }
+                else if (boardd[i] == "X" && boardd[j] == "X" && board[k] == "X")
+                {
+                    if (magicboard[i] + magicboard[j] + magicboard[k] == 15)
+                    {
+                        if (i != j && i != k && j != k)
+                        {
+                            return -1;
+                        }
+                       
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
 function algorithm(loadedboard, symbol)
 {
     let info = [];
@@ -17,6 +52,11 @@ function algorithm(loadedboard, symbol)
     for (let i = 0; i < empty.length; i++)
     {
         let currentinfo = {};
+        if (win(loadedboard, i) == symbol)
+        {
+            //blank
+        }
+
     }
 
     //subtract result from bad guy from 15
